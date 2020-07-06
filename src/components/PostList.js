5f78d2b8b0908b088cb18a3223dfb7adc5ab7633
James <james.hau@vybn.co.uk>
2020-07-06 17:30:07 +0100

    final commit.  Learning about mapping data from API calls onto the screen using middleware and reducers to process the data.  Add reducer class and functionality to display the data as intended

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -12,9 +12,31 @@ class PostList extends React.Component{
         this.props.fetchPosts();
     }
 
+    renderList(){
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large icon"/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+
+    }
+
     render () {
-        return <div>Post List</div>
+        return <div>{this.renderList()}</div>
     }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts}
+
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
